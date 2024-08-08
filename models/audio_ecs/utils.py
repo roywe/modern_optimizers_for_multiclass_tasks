425afd65ee9a27c17ef460dfdfa6a6aa646bafd4
roywe <roy.veber@gmail.com>
2024-08-09 00:39:04 +0300

    files audio

--- models/audio_ecs/utils.py
+++ models/audio_ecs/utils.py
@@ -9,9 +9,12 @@ from pathlib import Path
 import torchaudio
 import numpy as np
 from transformers import BertModel, BertConfig
+from torch.utils.data import TensorDataset, DataLoader
+import pandas as pd
 
-DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
 LOSS_CRITERIA = nn.CrossEntropyLoss()
+ONLY_10_LABELS = True
 
 class BertForSequenceLabeling(nn.Module):
     def __init__(self, num_labels=50):
@@ -31,6 +34,86 @@ class BertForSequenceLabeling(nn.Module):
         # Prepare logits for sequence labeling
         logits = self.bert.pooler.dense(sequence_output)  # Shape: (batch_size, max_length, num_labels)
         return logits
+
+class AudioDataset(Dataset):
+    def __init__(self, kind='train', path=os.path.join(Path(os.getcwd()).parent.parent,"datasets/ESC-50-master/audio"), labels_amount = 50, is_only_10=ONLY_10_LABELS):
+        if is_only_10:
+            meta_data_file = os.path.join(Path(os.getcwd()).parent.parent,"datasets/ESC-50-master/meta",'esc50.csv')
+            dataset = pd.read_csv(meta_data_file)
+            valid_files = list(dataset[dataset['esc10']]['filename'])
+            # labels_amount = 10
+            
+        self.labels_amount = labels_amount
+        if kind=='train':
+            files = Path(path).glob('[1-3]-*')
+            self.items = [(str(file), file.name.split('-')[-1].replace('.wav', '')) for file in files]
+        
+        if kind=='val':
+            files = Path(path).glob('4-*')
+            self.items = [(str(file), file.name.split('-')[-1].replace('.wav', '')) for file in files]
+            
+        if kind=='test':
+            files = Path(path).glob('5-*')
+            self.items = [(str(file), file.name.split('-')[-1].replace('.wav', '')) for file in files]
+        
+        if is_only_10:
+            self.items = [file for file in self.items if Path(file[0]).name in valid_files]
+        
+    def __len__(self):
+        return len(self.items)
+
+    def __getitem__(self, idx):
+
+        filename, label = self.items[idx]
+        label = torch.tensor(int(label), dtype=torch.long)
+        label_one_hot = torch.nn.functional.one_hot(label, num_classes=self.labels_amount)
+        data_tensor, rate = torchaudio.load(filename)
+        wav_to_vec_rate = 16000
+        transform = torchaudio.transforms.Resample(orig_freq=rate, new_freq=wav_to_vec_rate)
+        resampled_data_tensor = transform(data_tensor)
+                
+        # Convert waveform to feature representation if needed (e.g., MFCC, spectrogram)
+        # Here we'll just return the raw waveform for simplicity
+        return resampled_data_tensor.squeeze(0)[:40000], label_one_hot  # [sequence_length]
+
+
+def create_dataset(kind='train', path=os.path.join(Path(os.getcwd()).parent.parent,"datasets/ESC-50-master/audio"), labels_amount = 50):
+    preprocess_model = torchaudio.pipelines.WAV2VEC2_BASE.get_model().to(DEVICE)
+    
+    if kind=='train':
+            files = Path(path).glob('[1-3]-*')
+            items = [(str(file), file.name.split('-')[-1].replace('.wav', '')) for file in files]
+        
+    if kind=='val':
+        files = Path(path).glob('4-*')
+        items = [(str(file), file.name.split('-')[-1].replace('.wav', '')) for file in files]
+        
+    if kind=='test':
+        files = Path(path).glob('5-*')
+        items = [(str(file), file.name.split('-')[-1].replace('.wav', '')) for file in files]
+    
+    x_train, y_train = [],[]   
+    for index in range(len(items)):
+        filename, label = items[index]
+        label = torch.tensor(int(label), dtype=torch.long)
+        label_one_hot = torch.nn.functional.one_hot(label, num_classes=labels_amount)
+        data_tensor, rate = torchaudio.load(filename)
+        wav_to_vec_rate = 16000
+        transform = torchaudio.transforms.Resample(orig_freq=rate, new_freq=wav_to_vec_rate)
+        resampled_data_tensor = transform(data_tensor)
+        
+        with torch.no_grad():
+            preprocess_model.eval()
+            resampled_data_tensor = torch.Tensor(resampled_data_tensor).to(DEVICE)
+            tmp, _ = preprocess_model.extract_features(resampled_data_tensor)
+            tmp = tmp[6]
+            tmp = tmp.data.cpu().numpy()[0]
+        x_train.append(tmp)
+        y_train.append(label_one_hot)
+        
+    # tmp = data_tensor[0,0:80000]
+    dataset = TensorDataset(torch.tensor(np.array(x_train) , dtype=float), torch.tensor(np.array(y_train) , dtype=float))
+    return dataset
 
 
 class DataGenerator(Dataset):
