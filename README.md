a263eebd48a03e81130d202ac1b63b67d63405bc
roywe <roy.veber@gmail.com>
2024-08-09 17:10:30 +0300

    ecs keep moving forward - train on 100 trails (or 10 minutes) results in 20240809, 64 batch size for 100 epochs also readme and git ignore

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
-# 046211_modern_optimizers_for_pretrained_models
+# 046211_modern_optimizers_for_multiclass_tasks
+
+# 046211 modern optimizers for multiclass tasks
+Welcome to the to our project repository.
+This repository houses the code and resources for checking new optimizers performances on one nlp and one vision task.
+Our main goal is to check the optimizers validating their articles.
+
+
+## Overview
+In the recent years, new optimization methods arise.
+while Adam is well known and used, different optimization technique claims that they are superior for specific cases.
+Adan and Madgrad are two of those techniques. 
+Adan - (https://arxiv.org/pdf/2208.06677) using nestrov momuntum show Sota results for vision transformer, bert and gpt and promsing results for RL method.
+further download and documentation is here: (https://github.com/lucidrains/Adan-pytorch/tree/main)
+Madgrad - (https://arxiv.org/pdf/2101.11075) - ................. 
+further download and documentation is here: (https://github.com/facebookresearch/madgrad)
+
+
+## Methodology
+In this project, we tested the optimizers credability for NLP task and vision task.
+We tested SGD, Adam, Madgrad and Adan with the 2 tasks and checked if loss and accuracy is behave as the article suggest.
+Adan claims to be faster to converge vs other optimizors
+Madgrad ......
+We chose the following tasks:
+-  ECS-50 Audio Dataset for multiclass NLP classification using Hubert
+-  image............................ 
+
+
+## Data
+The datasets for the tasks include:
+ECS - (https://github.com/karoldvl/ESC-50) containing 50 classes of 40 audio records of 5 seconds each
+for this project we used only 10 classes (that are in ECS-10). In this github we can see the top models performances. 
+Our goal was to compare different optimizers and not becoming SOTA for this task/
+
+Images - 
+
+<br>
+<br>
+
+<!-- ![WhatsApp Image 2024-04-07 at 21 04 28_e74cb264](https://github.com/DanielLevi6/046211-Deep-Learning/assets/88712194/c96e8a3b-c3f8-4f27-8d41-56e13096ba48) -->
+
+## Model
+ECS - we used well known model for audio named Hubert - (https://arxiv.org/pdf/2106.07447)
+this is a Bert similar model using CNN for the first layers and projection after the end layers
+
+Images - .................
+
+
+## Repository Structure
+- **optimizers:** containing simple check we did with the optimizers
+- **models:**  containing code for the two task - audio_ecs and imagenet
+- 
+
+
+## How to Use
+To utilize our earthquake prediction model, follow these steps:
+1. **Clone the Repository:** Clone this repository to your local machine.
+2. **Install Dependencies:** Ensure all necessary dependencies are installed.
+3. **Prepare Data:** If using custom data, format it appropriately and replace the existing dataset.
+4. **Run the Model:** Execute provided scripts in the `code` directory.
+5. **Evaluate Results:** Examine predictions and evaluate model performance.
+
+## Libraries Used
+the Requirements.txt file in this repo can be used to download all of the required libraries.
+
+## Results Summary
+
+
+## Conclusion
+
+
+
+## References
+
+
+
+## Future Work
+
+
+## Contact
+For inquiries or further information, please contact roy.weber@campus.technion.ac.il, tomer.rudich@campus.technion.ac.il
+Thank you for your interest in our Optimizers project.
--- models/audio_ecs/utils.py
+++ models/audio_ecs/utils.py
@@ -8,32 +8,13 @@ import os
 from pathlib import Path
 import torchaudio
 import numpy as np
-from transformers import BertModel, BertConfig
-from torch.utils.data import TensorDataset, DataLoader
 import pandas as pd
 
-DEVICE = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device('cuda:4' if torch.cuda.is_available() else 'cpu')
 LOSS_CRITERIA = nn.CrossEntropyLoss()
 ONLY_10_LABELS = True
 
-class BertForSequenceLabeling(nn.Module):
-    def __init__(self, num_labels=50):
-        super(BertForSequenceLabeling, self).__init__()
-        self.bert = BertModel.from_pretrained('bert-base-uncased')
-        set_parameter_requires_grad(self.bert, feature_extracting=True)
-        end_input_features = self.bert.pooler.dense.in_features
-        self.bert.pooler.dense = nn.Linear(end_input_features,num_labels)
-        self.num_labels = num_labels  # Store num_labels as an instance variable
 
-    def forward(self, input_ids, attention_mask=None):
-        # Transform the embeddings to BERT-compatible dimensions
-        # Pass through BERT
-        outputs = self.bert(inputs_embeds=input_ids, attention_mask=attention_mask)
-        sequence_output = outputs.last_hidden_state  # Shape: (batch_size, max_length, hidden_size)
-        
-        # Prepare logits for sequence labeling
-        logits = self.bert.pooler.dense(sequence_output)  # Shape: (batch_size, max_length, num_labels)
-        return logits
 
 class AudioDataset(Dataset):
     def __init__(self, kind='train', path=os.path.join(Path(os.getcwd()).parent.parent,"datasets/ESC-50-master/audio"), labels_amount = 50, is_only_10=ONLY_10_LABELS):
@@ -70,122 +51,11 @@ class AudioDataset(Dataset):
         data_tensor, rate = torchaudio.load(filename)
         wav_to_vec_rate = 16000
         transform = torchaudio.transforms.Resample(orig_freq=rate, new_freq=wav_to_vec_rate)
-        resampled_data_tensor = transform(data_tensor)
-                
+        data_tensor = transform(data_tensor)
+        data_tensor = data_tensor.squeeze(0)
         # Convert waveform to feature representation if needed (e.g., MFCC, spectrogram)
         # Here we'll just return the raw waveform for simplicity
-        return resampled_data_tensor.squeeze(0)[:40000], label_one_hot  # [sequence_length]
-
-
-def create_dataset(kind='train', path=os.path.join(Path(os.getcwd()).parent.parent,"datasets/ESC-50-master/audio"), labels_amount = 50):
-    preprocess_model = torchaudio.pipelines.WAV2VEC2_BASE.get_model().to(DEVICE)
-    
-    if kind=='train':
-            files = Path(path).glob('[1-3]-*')
-            items = [(str(file), file.name.split('-')[-1].replace('.wav', '')) for file in files]
-        
-    if kind=='val':
-        files = Path(path).glob('4-*')
-        items = [(str(file), file.name.split('-')[-1].replace('.wav', '')) for file in files]
-        
-    if kind=='test':
-        files = Path(path).glob('5-*')
-        items = [(str(file), file.name.split('-')[-1].replace('.wav', '')) for file in files]
-    
-    x_train, y_train = [],[]   
-    for index in range(len(items)):
-        filename, label = items[index]
-        label = torch.tensor(int(label), dtype=torch.long)
-        label_one_hot = torch.nn.functional.one_hot(label, num_classes=labels_amount)
-        data_tensor, rate = torchaudio.load(filename)
-        wav_to_vec_rate = 16000
-        transform = torchaudio.transforms.Resample(orig_freq=rate, new_freq=wav_to_vec_rate)
-        resampled_data_tensor = transform(data_tensor)
-        
-        with torch.no_grad():
-            preprocess_model.eval()
-            resampled_data_tensor = torch.Tensor(resampled_data_tensor).to(DEVICE)
-            tmp, _ = preprocess_model.extract_features(resampled_data_tensor)
-            tmp = tmp[6]
-            tmp = tmp.data.cpu().numpy()[0]
-        x_train.append(tmp)
-        y_train.append(label_one_hot)
-        
-    # tmp = data_tensor[0,0:80000]
-    dataset = TensorDataset(torch.tensor(np.array(x_train) , dtype=float), torch.tensor(np.array(y_train) , dtype=float))
-    return dataset
-
-
-class DataGenerator(Dataset):
-    def __init__(self, path, kind='train', labels_amount=50):
-        
-        self.model = torchaudio.pipelines.WAV2VEC2_BASE.get_model().to(DEVICE)
-        # print(self.model)
-        if kind=='train':
-            files = Path(path).glob('[1-3]-*')
-            self.items = [(str(file), file.name.split('-')[-1].replace('.wav', '')) for file in files]
-        
-        if kind=='val':
-            files = Path(path).glob('4-*')
-            self.items = [(str(file), file.name.split('-')[-1].replace('.wav', '')) for file in files]
-            
-        if kind=='test':
-            files = Path(path).glob('5-*')
-            self.items = [(str(file), file.name.split('-')[-1].replace('.wav', '')) for file in files]
-            
-        self.length = len(self.items)
-        self.labels_amount = labels_amount
-        
-    def __getitem__(self, index):
-        filename, label = self.items[index]
-        label = torch.tensor(int(label), dtype=torch.long)
-        label_one_hot = torch.nn.functional.one_hot(label, num_classes=self.labels_amount)
-        data_tensor, rate = torchaudio.load(filename)
-        wav_to_vec_rate = 16000
-        transform = torchaudio.transforms.Resample(orig_freq=rate, new_freq=wav_to_vec_rate)
-        resampled_data_tensor = transform(data_tensor)
-        
-        with torch.no_grad():
-            self.model.eval()
-            resampled_data_tensor = torch.Tensor(resampled_data_tensor).to(DEVICE)
-            tmp, _ = self.model.extract_features(resampled_data_tensor)
-            tmp = tmp[6]
-            tmp = tmp.data.cpu().numpy()[0]
-        
-        # tmp = data_tensor[0,0:80000]
-        return (tmp, label_one_hot)
-    
-    def __len__(self):
-        return self.length
-
-
-def set_parameter_requires_grad(model, feature_extracting=False):
-    # approach 1
-    if feature_extracting:
-        # frozen model
-        model.requires_grad_(False)
-    else:
-        # fine-tuning
-        model.requires_grad_(True)
-        
-    # approach 2
-    if feature_extracting:
-        # frozen model
-        for param in model.parameters():
-            param.requires_grad = False
-    else:
-        # fine-tuning
-        for param in model.parameters():
-            param.requires_grad = True
-    
-
-def preparte_data_loader(mode='train', batch_size = 64, audio_folder= os.path.join(Path(os.getcwd()).parent.parent,"datasets/ESC-50-master/audio")):
-    train_data = DataGenerator(audio_folder, kind=mode)
-    # test_data = DataGenerator(audio_folder, kind='test')
-
-    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
-    # test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
-    return train_loader
-
+        return data_tensor[:40000], label_one_hot  # [sequence_length]
+# [:40000]
 if __name__ == '__main__':
     print()
